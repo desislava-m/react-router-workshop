43fb1e1c21258a6f8661c049daae165d4014eb5b
Dessie <desislava.m.alexandrova@gmail.com>
2025-06-13 18:20:11 +0300

    Creating login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "../components/Header"
 import { Routes, Route } from 'react-router-dom';
 import  Home  from "../components/Home"
 import Pricing from "../components/Pricing";
+import Login from "../components/Login"
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/pricing' element={<Pricing />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
     </>
   )
